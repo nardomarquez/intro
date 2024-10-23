9a8ac24aff38e5157190548fbd489400982c7bdf
Rennard Marquez <marquezrennard@gmail.com>
2024-10-23 19:34:42 +0800

    add marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,51 @@
+"use client";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { horizontalLoop } from "@/lib/helperFunctions";
 import Scene from "@/components/Scene";
 
+gsap.registerPlugin(useGSAP);
+
 export default function Home() {
+  const marqueeRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const marqueeItems = document.querySelectorAll(".marquee > div");
+
+      const loop = horizontalLoop(marqueeItems, {
+        duration: 10,
+        ease: "none",
+        repeat: -1,
+      });
+    },
+    { scope: marqueeRef }
+  );
+
   return (
     <div className="flex flex-col justify-between h-screen p-6">
       <main className="flex-1 flex flex-col justify-between my-8">
         <div className="grid grid-cols-4 gap-6">
           <p className="text-4xl col-[3/5] font-bold leading-[110%] tracking-[-0.04em] text-pretty">
-            Hello. I’m Nardo, a Creative Developer based in Manila. I craft
+            Hello. I&apos;m Nardo, a Creative Developer based in Manila. I craft
             bespoke digital experiences that elevate brands.
           </p>
         </div>
       </main>
 
-      <div className="overflow-hidden" aria-hidden="true">
+      <div className="overflow-hidden" aria-hidden="true" ref={marqueeRef}>
         <div className="animate-marquee whitespace-nowrap">
-          <p className="mix-blend-multiply text-[10rem] font-bold uppercase -tracking-wider inline-block leading-none align-baseline">
-            Nardo
-            <span className="text-[#B2B7C9]">你好</span>
-            Marquez
-            <span className="text-[#B2B7C9]">Ciao</span>
-            Nardo
-            <span className="text-[#B2B7C9]">Hello</span>
-            Marquez
+          <p className="marquee flex mix-blend-multiply text-[10rem] font-bold uppercase -tracking-wider leading-none align-baseline">
+            <div>Nardo</div>
+            <div className="text-[#B2B7C9]">你好</div>
+            <div>Marquez</div>
+            <div className="text-[#B2B7C9]">Hello</div>
+            <div>Nardo</div>
+            <div className="text-[#B2B7C9]">やっほー</div>
+            <div>Marquez</div>
+            <div className="text-[#B2B7C9]">Ciao</div>
           </p>
           <p
             className="text-[#797e8e] font-semibold uppercase leading-[110%] text-justify w-full text-pretty [text-align-last:justify]"
